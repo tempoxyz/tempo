266f59180b1b2fd6ea5052a3898cba09840160ae
t11s <t11s@mit.edu>
2025-08-06 02:18:20 -0700

    perf: fixed slices

--- crates/precompiles/src/contracts/storage/slots.rs
+++ crates/precompiles/src/contracts/storage/slots.rs
@@ -6,12 +6,24 @@ pub const fn to_u256(x: u64) -> U256 {
     U256::from_limbs(limbs)
 }
 
+pub const fn pad_to_32(x: &[u8]) -> [u8; 32] {
+    let mut buf = [0u8; 32];
+    let mut i = 0;
+    // Note: This is not idiomatic but it's the cleanest
+    // way to make this function const as far as I can tell.
+    while i < x.len() && i < 32 {
+        buf[i] = x[i];
+        i += 1;
+    }
+    buf
+}
+
 /// Compute storage slot for a mapping
 pub fn mapping_slot<T: AsRef<[u8]>>(key: T, mapping_slot: U256) -> U256 {
-    let mut data = Vec::new();
-    data.extend_from_slice(key.as_ref());
-    data.extend_from_slice(&mapping_slot.to_le_bytes::<32>());
-    U256::from_be_bytes(keccak256(&data).0)
+    let mut buf = [0u8; 64];
+    buf[..32].copy_from_slice(&pad_to_32(key.as_ref()));
+    buf[32..].copy_from_slice(&mapping_slot.to_le_bytes::<32>());
+    U256::from_be_bytes(keccak256(&buf).0)
 }
 
 /// Compute storage slot for a double mapping (mapping\[key1\]\[key2\])
@@ -21,10 +33,10 @@ pub fn double_mapping_slot<T: AsRef<[u8]>, U: AsRef<[u8]>>(
     base_slot: U256,
 ) -> U256 {
     let intermediate_slot = mapping_slot(key1, base_slot);
-    let mut data = Vec::new();
-    data.extend_from_slice(key2.as_ref());
-    data.extend_from_slice(&intermediate_slot.to_be_bytes::<32>());
-    U256::from_be_bytes(keccak256(&data).0)
+    let mut buf = [0u8; 64];
+    buf[..32].copy_from_slice(&pad_to_32(key2.as_ref()));
+    buf[32..].copy_from_slice(&intermediate_slot.to_be_bytes::<32>());
+    U256::from_be_bytes(keccak256(&buf).0)
 }
 
 #[cfg(test)]
