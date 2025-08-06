70ba155e3e939514f3910ec520710ad4246b0db8
t11s <t11s@mit.edu>
2025-08-05 23:30:53 -0700

    chore: rm more stale files

--- crates/precompiles/src/contracts/mod.rs
+++ crates/precompiles/src/contracts/mod.rs
@@ -1,6 +1,5 @@
 pub mod erc20;
 pub mod erc20_factory;
-pub mod parsing;
 pub mod roles;
 pub mod storage;
 pub mod types;
@@ -8,7 +7,5 @@ pub mod utils;
 
 pub use erc20::ERC20Token;
 pub use erc20_factory::ERC20Factory;
-pub use storage::evm::EvmStorageProvider;
-pub use storage::hashmap::HashMapStorageProvider;
-pub use storage::StorageProvider;
-pub use types::{IERC20Factory, IERC20};
+pub use storage::{StorageProvider, evm::EvmStorageProvider, hashmap::HashMapStorageProvider};
+pub use types::{IERC20, IERC20Factory};
