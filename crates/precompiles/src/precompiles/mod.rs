dfc24b518558f6043a6fc46aa14eea9880713bf4
0xKitsune <0xKitsune@protonmail.com>
2025-08-07 13:30:42 -0400

    chore: clippy

--- crates/precompiles/src/precompiles/mod.rs
+++ crates/precompiles/src/precompiles/mod.rs
@@ -1,13 +1,13 @@
 use alloy::primitives::Address;
 use reth::revm::{
-    Inspector,
     precompile::{PrecompileError, PrecompileOutput, PrecompileResult},
     primitives::hardfork::SpecId,
+    Inspector,
 };
 use reth_evm::{
-    Database, EthEvm, Evm,
     eth::EthEvmContext,
     precompiles::{DynPrecompile, PrecompilesMap},
+    Database, EthEvm, Evm,
 };
 
 mod dispatch;
@@ -16,11 +16,11 @@ pub mod tip20_factory;
 pub mod tip403_registry;
 
 use crate::contracts::{
-    EvmStorageProvider, TIP20Factory, TIP20Token, TIP403Registry,
     utils::{
-        FACTORY_ADDRESS, TIP403_REGISTRY_ADDRESS, address_is_token_address,
-        address_to_token_id_unchecked,
+        address_is_token_address, address_to_token_id_unchecked, FACTORY_ADDRESS,
+        TIP403_REGISTRY_ADDRESS,
     },
+    EvmStorageProvider, TIP20Factory, TIP20Token, TIP403Registry,
 };
 
 const METADATA_GAS: u64 = 50;
@@ -39,11 +39,11 @@ pub fn extend_tempo_precompiles<DB: Database, I: Inspector<EthEvmContext<DB>>>(
         let precompiles = evm.precompiles_mut();
         precompiles.set_precompile_lookup(move |address: &Address| {
             if address_is_token_address(address) {
-                Some(TIP20Precompile::new(address, chain_id))
+                Some(TIP20Precompile::create(address, chain_id))
             } else if *address == FACTORY_ADDRESS {
-                Some(TIP20FactoryPrecompile::new(chain_id))
+                Some(TIP20FactoryPrecompile::create(chain_id))
             } else if *address == TIP403_REGISTRY_ADDRESS {
-                Some(TIP403RegistryPrecompile::new(chain_id))
+                Some(TIP403RegistryPrecompile::create(chain_id))
             } else {
                 None
             }
@@ -53,7 +53,7 @@ pub fn extend_tempo_precompiles<DB: Database, I: Inspector<EthEvmContext<DB>>>(
 
 pub struct TIP20Precompile;
 impl TIP20Precompile {
-    pub fn new(address: &Address, chain_id: u64) -> DynPrecompile {
+    pub fn create(address: &Address, chain_id: u64) -> DynPrecompile {
         let token_id = address_to_token_id_unchecked(address);
         DynPrecompile::new(move |input| {
             TIP20Token::new(
@@ -68,7 +68,7 @@ impl TIP20Precompile {
 pub struct TIP20FactoryPrecompile;
 
 impl TIP20FactoryPrecompile {
-    pub fn new(chain_id: u64) -> DynPrecompile {
+    pub fn create(chain_id: u64) -> DynPrecompile {
         DynPrecompile::new(move |input| {
             TIP20Factory::new(&mut EvmStorageProvider::new(input.internals, chain_id))
                 .call(input.data, &input.caller)
@@ -79,7 +79,7 @@ impl TIP20FactoryPrecompile {
 pub struct TIP403RegistryPrecompile;
 
 impl TIP403RegistryPrecompile {
-    pub fn new(chain_id: u64) -> DynPrecompile {
+    pub fn create(chain_id: u64) -> DynPrecompile {
         DynPrecompile::new(move |input| {
             TIP403Registry::new(&mut EvmStorageProvider::new(input.internals, chain_id))
                 .call(input.data, &input.caller)
