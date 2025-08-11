d3d044474c64ae419566b4a823df2f03e6c9f03f
0xKitsune <0xKitsune@protonmail.com>
2025-08-11 01:11:11 -0400

    bench: tip403 benchmarks

--- crates/precompiles/benches/tempo_precompiles.rs
+++ crates/precompiles/benches/tempo_precompiles.rs
@@ -5,7 +5,9 @@ use std::hint::black_box;
 use tempo_precompiles::contracts::{
     storage::hashmap::HashMapStorageProvider,
     tip20::{ISSUER_ROLE, TIP20Token},
-    types::ITIP20,
+    tip20_factory::TIP20Factory,
+    tip403_registry::TIP403Registry,
+    types::{ITIP20, ITIP20Factory, ITIP403Registry},
 };
 
 fn tip20_metadata(c: &mut Criterion) {
@@ -210,28 +212,203 @@ fn tip20_mutate(_c: &mut Criterion) {
     todo!()
 }
 
-fn tip20_factory_metadata(_c: &mut Criterion) {
-    todo!()
+
+fn tip20_factory_view(c: &mut Criterion) {
+    c.bench_function("tip20_factory_token_id_counter", |b| {
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut factory = TIP20Factory::new(&mut storage);
+
+        b.iter(|| {
+            let f = black_box(&mut factory);
+            f.token_id_counter();
+        });
+    });
 }
 
-fn tip20_factory_view(_c: &mut Criterion) {
-    todo!()
+fn tip20_factory_mutate(c: &mut Criterion) {
+    c.bench_function("tip20_factory_create_token", |b| {
+        let sender = Address::from([1u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut factory = TIP20Factory::new(&mut storage);
+        
+        let create_call = ITIP20Factory::createTokenCall {
+            name: "Test Token".to_string(),
+            symbol: "TEST".to_string(),
+            currency: "USD".to_string(),
+            admin: sender,
+        };
+
+        b.iter(|| {
+            let f = black_box(&mut factory);
+            f.create_token(&sender, create_call.clone()).unwrap();
+        });
+    });
 }
 
-fn tip20_factory_mutate(_c: &mut Criterion) {
-    todo!()
+
+fn tip403_registry_view(c: &mut Criterion) {
+    c.bench_function("tip403_registry_policy_id_counter", |b| {
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.policy_id_counter();
+        });
+    });
+
+    c.bench_function("tip403_registry_policy_data", |b| {
+        let admin = Address::from([0u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+        let policy_id = registry.create_policy(
+            &admin,
+            ITIP403Registry::createPolicyCall {
+                adminPolicyId: 1,
+                policyType: ITIP403Registry::PolicyType::WHITELIST,
+            },
+        ).unwrap();
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.policy_data(ITIP403Registry::policyDataCall { policyId: policy_id });
+        });
+    });
+
+    c.bench_function("tip403_registry_is_authorized", |b| {
+        let admin = Address::from([0u8; 20]);
+        let user = Address::from([1u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+        let policy_id = registry.create_policy(
+            &admin,
+            ITIP403Registry::createPolicyCall {
+                adminPolicyId: 1,
+                policyType: ITIP403Registry::PolicyType::WHITELIST,
+            },
+        ).unwrap();
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.is_authorized(ITIP403Registry::isAuthorizedCall { policyId: policy_id, user });
+        });
+    });
 }
 
-fn tip403_registry_metadata(_c: &mut Criterion) {
-    todo!()
-}
+fn tip403_registry_mutate(c: &mut Criterion) {
+    c.bench_function("tip403_registry_create_policy", |b| {
+        let admin = Address::from([0u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
 
-fn tip403_registry_view(_c: &mut Criterion) {
-    todo!()
-}
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.create_policy(
+                &admin,
+                ITIP403Registry::createPolicyCall {
+                    adminPolicyId: 1,
+                    policyType: ITIP403Registry::PolicyType::WHITELIST,
+                },
+            ).unwrap();
+        });
+    });
 
-fn tip403_registry_mutate(_c: &mut Criterion) {
-    todo!()
+    c.bench_function("tip403_registry_create_policy_with_accounts", |b| {
+        let admin = Address::from([0u8; 20]);
+        let account1 = Address::from([1u8; 20]);
+        let account2 = Address::from([2u8; 20]);
+        let accounts = vec![account1, account2];
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.create_policy_with_accounts(
+                &admin,
+                ITIP403Registry::createPolicyWithAccountsCall {
+                    adminPolicyId: 1,
+                    policyType: ITIP403Registry::PolicyType::WHITELIST,
+                    accounts: accounts.clone(),
+                },
+            ).unwrap();
+        });
+    });
+
+    c.bench_function("tip403_registry_set_policy_admin", |b| {
+        let admin = Address::from([0u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+        let policy_id = registry.create_policy(
+            &admin,
+            ITIP403Registry::createPolicyCall {
+                adminPolicyId: 1,
+                policyType: ITIP403Registry::PolicyType::WHITELIST,
+            },
+        ).unwrap();
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.set_policy_admin(
+                &admin,
+                ITIP403Registry::setPolicyAdminCall {
+                    policyId: policy_id,
+                    adminPolicyId: 1,
+                },
+            ).unwrap();
+        });
+    });
+
+    c.bench_function("tip403_registry_modify_policy_whitelist", |b| {
+        let admin = Address::from([0u8; 20]);
+        let user = Address::from([1u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+        let policy_id = registry.create_policy(
+            &admin,
+            ITIP403Registry::createPolicyCall {
+                adminPolicyId: 1,
+                policyType: ITIP403Registry::PolicyType::WHITELIST,
+            },
+        ).unwrap();
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.modify_policy_whitelist(
+                &admin,
+                ITIP403Registry::modifyPolicyWhitelistCall {
+                    policyId: policy_id,
+                    account: user,
+                    allowed: true,
+                },
+            ).unwrap();
+        });
+    });
+
+    c.bench_function("tip403_registry_modify_policy_blacklist", |b| {
+        let admin = Address::from([0u8; 20]);
+        let user = Address::from([1u8; 20]);
+        let mut storage = HashMapStorageProvider::new(1);
+        let mut registry = TIP403Registry::new(&mut storage);
+        let policy_id = registry.create_policy(
+            &admin,
+            ITIP403Registry::createPolicyCall {
+                adminPolicyId: 1,
+                policyType: ITIP403Registry::PolicyType::BLACKLIST,
+            },
+        ).unwrap();
+
+        b.iter(|| {
+            let r = black_box(&mut registry);
+            r.modify_policy_blacklist(
+                &admin,
+                ITIP403Registry::modifyPolicyBlacklistCall {
+                    policyId: policy_id,
+                    account: user,
+                    restricted: true,
+                },
+            ).unwrap();
+        });
+    });
 }
 
 criterion_group!(
@@ -239,10 +416,8 @@ criterion_group!(
     tip20_metadata,
     tip20_view,
     tip20_mutate,
-    tip20_factory_metadata,
     tip20_factory_view,
     tip20_factory_mutate,
-    tip403_registry_metadata,
     tip403_registry_view,
     tip403_registry_mutate
 );
