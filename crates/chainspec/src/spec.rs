9a7ea3eee3a1605967ed9a6bdfaee4a677938a7c
0xKitsune <77890308+0xKitsune@users.noreply.github.com>
2025-08-12 12:54:48 -0400

    chore: update expect message
    Co-authored-by: Richard Janis Goldschmidt <github@aberrat.io>

--- crates/chainspec/src/spec.rs
+++ crates/chainspec/src/spec.rs
@@ -33,7 +33,7 @@ impl ChainSpecParser for TempoChainSpecParser {
 
 pub static ADAGIO: LazyLock<Arc<ChainSpec>> = LazyLock::new(|| {
     let _genesis: Genesis = serde_json::from_str(include_str!("./genesis/adagio.json"))
-        .expect("Can't deserialize Adagio genesis json");
+        .expect("`../res/genesis/adagio.json` must be present and deserializable");
     let _hardforks: ChainHardforks = EthereumHardfork::mainnet().into();
     let mut spec = ChainSpec {
         chain: Chain::from(1234),
