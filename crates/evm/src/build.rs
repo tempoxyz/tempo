8f5f9eb3e68867a46765c518aaac1bf731d874b6
0xKitsune <0xKitsune@protonmail.com>
2025-08-13 13:28:34 -0400

    wip: generic block assembler

--- crates/evm/src/build.rs
+++ crates/evm/src/build.rs
@@ -46,6 +46,65 @@ impl<ChainSpec: EthereumHardforks> TempoBlockAssembler<ChainSpec> {
         &self,
         input: BlockAssemblerInput<'_, '_, F, H>,
     ) -> Result<Block<F::Transaction>, BlockExecutionError> {
+        let BlockAssemblerInput {
+            evm_env,
+            execution_ctx: ctx,
+            parent,
+            transactions,
+            output:
+                BlockExecutionResult {
+                    receipts,
+                    requests,
+                    gas_used,
+                },
+            state_root,
+            ..
+        } = input;
+
+        let timestamp = evm_env.block_env.timestamp.saturating_to();
+        let transactions_root = proofs::calculate_transaction_root(&transactions);
+
+        // TODO: receipts root
+
+        let logs_bloom = logs_bloom(receipts.iter().flat_map(|r| r.logs()));
+        let withdrawals = self
+            .chain_spec
+            .is_shanghai_active_at_timestamp(timestamp)
+            .then(|| ctx.withdrawals.map(|w| w.into_owned()).unwrap_or_default());
+
+        let withdrawals_root = withdrawals
+            .as_deref()
+            .map(|w| proofs::calculate_withdrawals_root(w));
+        let requests_hash = self
+            .chain_spec
+            .is_prague_active_at_timestamp(timestamp)
+            .then(|| requests.requests_hash());
+
+        // let mut excess_blob_gas = None;
+        // let mut blob_gas_used = None;
+
+        // // only determine cancun fields when active
+        // if self.chain_spec.is_cancun_active_at_timestamp(timestamp) {
+        //     blob_gas_used = Some(
+        //         transactions
+        //             .iter()
+        //             .map(|tx| tx.blob_gas_used().unwrap_or_default())
+        //             .sum(),
+        //     );
+        //     excess_blob_gas = if self
+        //         .chain_spec
+        //         .is_cancun_active_at_timestamp(parent.timestamp)
+        //     {
+        //         parent.maybe_next_block_excess_blob_gas(
+        //             self.chain_spec.blob_params_at_timestamp(timestamp),
+        //         )
+        //     } else {
+        //         // for the first post-fork block, both parent.blob_gas_used and
+        //         // parent.excess_blob_gas are evaluated as 0
+        //         Some(alloy_eips::eip7840::BlobParams::cancun().next_block_excess_blob_gas(0, 0))
+        //     };
+        // }
+
         todo!()
     }
 }
