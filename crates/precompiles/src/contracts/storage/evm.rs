7f47812a2c6616d641891afcbecc6e38509b7cea
0xKitsune <0xKitsune@protonmail.com>
2025-08-15 16:48:44 -0400

    wip: test tempo evm

--- crates/precompiles/src/contracts/storage/evm.rs
+++ crates/precompiles/src/contracts/storage/evm.rs
@@ -45,3 +45,38 @@ impl<'a> StorageProvider for EvmStorageProvider<'a> {
             .map_or(U256::ZERO, |value| value.data)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy::primitives::{B256, LogData, address};
+    use reth::revm::{
+        Context, MainContext,
+        context::{ContextTr, Evm},
+        db::{CacheDB, EmptyDB, InMemoryDB},
+        inspector::NoOpInspector,
+    };
+    use reth_evm::{EthEvm, EthEvmFactory, EvmEnv, EvmFactory, precompiles::PrecompilesMap};
+
+    fn create_evm() -> EthEvm<CacheDB<EmptyDB>, NoOpInspector, PrecompilesMap> {
+        let db = CacheDB::new(EmptyDB::new());
+        let evm = EthEvmFactory::default().create_evm(db, EvmEnv::default());
+        evm
+    }
+
+    #[test]
+    fn test_sstore() {
+        let mut evm = create_evm();
+        // let evm_internals = EvmInternals::new(evm.journal(), evm.block);
+        // let mut provider = EvmStorageProvider::new(evm_internals, 1);
+    }
+
+    #[test]
+    fn test_sload() {}
+
+    #[test]
+    fn test_set_code() {}
+
+    #[test]
+    fn test_emit_event() {}
+}
