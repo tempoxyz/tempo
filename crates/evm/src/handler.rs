a1d3fba8a704ad161f3c08e119bec94357fb1cd8
Matthias Seitz <matthias.seitz@outlook.de>
2025-08-17 15:37:53 +0200

    feat: add temp handler

---
+++ crates/evm/src/handler.rs
@@ -0,0 +1,64 @@
+//! Tempo EVM Handler implementation.
+
+use reth::revm::handler::MainnetHandler;
+use reth_revm::{
+    Database, ExecuteEvm,
+    context::{ContextTr, JournalTr, result::HaltReason},
+    handler::{EvmTr, EvmTrError, FrameResult, FrameTr, Handler, post_execution},
+    interpreter::interpreter_action::FrameInit,
+    state::EvmState,
+};
+
+/// Optimism handler extends the [`Handler`] with Optimism specific logic.
+#[derive(Debug, Clone)]
+pub struct TempoEvmHandler<EVM, ERROR, FRAME> {
+    /// The regular ethereum handler implementation, used to forward equivalent logic.
+    mainnet: MainnetHandler<EVM, ERROR, FRAME>,
+    /// Phantom data to avoid type inference issues.
+    _phantom: core::marker::PhantomData<(EVM, ERROR, FRAME)>,
+}
+
+impl<EVM, ERROR, FRAME> TempoEvmHandler<EVM, ERROR, FRAME> {
+    /// Create a new [`TempoEvmHandler`] handler instance
+    pub fn new() -> Self {
+        Self {
+            mainnet: MainnetHandler::default(),
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<EVM, ERROR, FRAME> Default for TempoEvmHandler<EVM, ERROR, FRAME> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<EVM, ERROR, FRAME> Handler for TempoEvmHandler<EVM, ERROR, FRAME>
+where
+    EVM: EvmTr<Context: ContextTr<Journal: JournalTr<State = EvmState>>, Frame = FRAME>,
+    ERROR: EvmTrError<EVM>,
+    FRAME: FrameTr<FrameResult = FrameResult, FrameInit = FrameInit>,
+{
+    type Evm = EVM;
+    type Error = ERROR;
+    type HaltReason = HaltReason;
+
+    #[inline]
+    fn validate_against_state_and_deduct_caller(
+        &self,
+        evm: &mut Self::Evm,
+    ) -> Result<(), Self::Error> {
+        // TODO
+        self.mainnet.validate_against_state_and_deduct_caller(evm)
+    }
+
+    #[inline]
+    fn reward_beneficiary(
+        &self,
+        evm: &mut Self::Evm,
+        exec_result: &mut <<Self::Evm as EvmTr>::Frame as FrameTr>::FrameResult,
+    ) -> Result<(), Self::Error> {
+        self.mainnet.reward_beneficiary(evm, exec_result)
+    }
+}
