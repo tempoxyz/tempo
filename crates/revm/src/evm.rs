59e71191a7ecb6dd1948fcd7941988c68b77d37c
0xKitsune <0xKitsune@protonmail.com>
2025-08-20 15:53:19 -0400

    feat: revm tempo evm

---
+++ crates/revm/src/evm.rs
@@ -0,0 +1,149 @@
+use reth_revm::{
+    Database, Inspector,
+    context::{ContextError, ContextSetters, ContextTr, Evm, FrameStack},
+    handler::{
+        EthFrame, EthPrecompiles, EvmTr, FrameInitOrResult, FrameTr, ItemOrResult,
+        PrecompileProvider,
+        instructions::{EthInstructions, InstructionProvider},
+    },
+    inspector::{InspectorEvmTr, JournalExt},
+    interpreter::{InterpreterResult, interpreter::EthInterpreter},
+};
+
+/// Tempo EVM extends the [`revm::Evm`] type with Tempo specific types and logic.
+#[derive(Debug, Clone)]
+pub struct TempoEvm<
+    CTX,
+    INSP,
+    I = EthInstructions<EthInterpreter, CTX>,
+    // TODO: Tempo Precompiles
+    P = EthPrecompiles,
+    F = EthFrame<EthInterpreter>,
+>(
+    /// Inner EVM type.
+    pub Evm<CTX, INSP, I, P, F>,
+);
+
+impl<CTX, INSP, I, P> TempoEvm<CTX, INSP, I, P> {
+    /// Consumed self and returns a new Evm type with given Inspector.
+    pub fn with_inspector<OINSP>(self, inspector: OINSP) -> TempoEvm<CTX, OINSP, I, P> {
+        TempoEvm(self.0.with_inspector(inspector))
+    }
+
+    /// Consumes self and returns a new Evm type with given Precompiles.
+    pub fn with_precompiles<OP>(self, precompiles: OP) -> TempoEvm<CTX, INSP, I, OP> {
+        TempoEvm(self.0.with_precompiles(precompiles))
+    }
+
+    /// Consumes self and returns the inner Inspector.
+    pub fn into_inspector(self) -> INSP {
+        self.0.into_inspector()
+    }
+}
+
+impl<CTX, INSP, I, P> InspectorEvmTr for TempoEvm<CTX, INSP, I, P>
+where
+    CTX: ContextTr<Journal: JournalExt> + ContextSetters,
+    I: InstructionProvider<Context = CTX, InterpreterTypes = EthInterpreter>,
+    P: PrecompileProvider<CTX, Output = InterpreterResult>,
+    INSP: Inspector<CTX, I::InterpreterTypes>,
+{
+    type Inspector = INSP;
+
+    fn inspector(&mut self) -> &mut Self::Inspector {
+        &mut self.0.inspector
+    }
+
+    fn ctx_inspector(&mut self) -> (&mut Self::Context, &mut Self::Inspector) {
+        (&mut self.0.ctx, &mut self.0.inspector)
+    }
+
+    fn ctx_inspector_frame(
+        &mut self,
+    ) -> (&mut Self::Context, &mut Self::Inspector, &mut Self::Frame) {
+        (
+            &mut self.0.ctx,
+            &mut self.0.inspector,
+            self.0.frame_stack.get(),
+        )
+    }
+
+    fn ctx_inspector_frame_instructions(
+        &mut self,
+    ) -> (
+        &mut Self::Context,
+        &mut Self::Inspector,
+        &mut Self::Frame,
+        &mut Self::Instructions,
+    ) {
+        (
+            &mut self.0.ctx,
+            &mut self.0.inspector,
+            self.0.frame_stack.get(),
+            &mut self.0.instruction,
+        )
+    }
+}
+
+impl<CTX, INSP, I, P> EvmTr for TempoEvm<CTX, INSP, I, P, EthFrame<EthInterpreter>>
+where
+    CTX: ContextTr,
+    I: InstructionProvider<Context = CTX, InterpreterTypes = EthInterpreter>,
+    P: PrecompileProvider<CTX, Output = InterpreterResult>,
+{
+    type Context = CTX;
+    type Instructions = I;
+    type Precompiles = P;
+    type Frame = EthFrame<EthInterpreter>;
+
+    fn ctx(&mut self) -> &mut Self::Context {
+        &mut self.0.ctx
+    }
+
+    fn ctx_ref(&self) -> &Self::Context {
+        &self.0.ctx
+    }
+
+    fn ctx_instructions(&mut self) -> (&mut Self::Context, &mut Self::Instructions) {
+        (&mut self.0.ctx, &mut self.0.instruction)
+    }
+
+    fn ctx_precompiles(&mut self) -> (&mut Self::Context, &mut Self::Precompiles) {
+        (&mut self.0.ctx, &mut self.0.precompiles)
+    }
+
+    fn frame_stack(&mut self) -> &mut FrameStack<Self::Frame> {
+        &mut self.0.frame_stack
+    }
+
+    fn frame_init(
+        &mut self,
+        frame_input: <Self::Frame as FrameTr>::FrameInit,
+    ) -> Result<
+        ItemOrResult<&mut Self::Frame, <Self::Frame as FrameTr>::FrameResult>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_init(frame_input)
+    }
+
+    fn frame_run(
+        &mut self,
+    ) -> Result<
+        FrameInitOrResult<Self::Frame>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_run()
+    }
+
+    #[doc = " Returns the result of the frame to the caller. Frame is popped from the frame stack."]
+    #[doc = " Consumes the frame result or returns it if there is more frames to run."]
+    fn frame_return_result(
+        &mut self,
+        result: <Self::Frame as FrameTr>::FrameResult,
+    ) -> Result<
+        Option<<Self::Frame as FrameTr>::FrameResult>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_return_result(result)
+    }
+}
