ef48d0d7667ba37ca17b4753f59b7ab3006fd898
0xKitsune <0xKitsune@protonmail.com>
2025-08-21 13:47:50 -0400

    wip: todos

--- crates/node/tests/tip20_transfer.rs
+++ crates/node/tests/tip20_transfer.rs
@@ -60,36 +60,10 @@ async fn test_tip20_transfer() -> eyre::Result<()> {
     let fee_manager = IFeeManager::new(TIP_FEE_MANAGER_ADDRESS, provider);
     let user_fee_token = fee_manager.userTokens(caller).call().await?;
     dbg!(user_fee_token);
-    // dbg!(user_token);
-    // // TODO: craft tip20 tx
-    // let tx = TransactionRequest::default();
 
-    // provider.send_transaction(tx);
-    //
-    //     // <cast send 0x20c0000000000000000000000000000000000000 'transfer(address,uint256)' 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC 100000000 --private-key 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d --gas-limit 2000000 --gas-price 4000000000>
-    //     let raw = hex!(
-    //         "0x02f8ae820539800184ee6b2800831e84809420c000000000000000000000000000000000000080b844a9059cbb0000000000000000000000003c44cdddb6a900fa2b585dd299e03d12fa4293bc0000000000000000000000000000000000000000000000000000000005f5e100c001a07c453d4ffe1b391089656e70658aa839435e18a5edab6113076166035c7d7afca06f454ef1b016bbf55cc147f4b20cda2719c5be22169b9c5c31366bde0c546d67"
-    //     );
-    //
-    //     let tx = TransactionSigned::decode_2718_exact(&raw[..])?.try_into_recovered()?;
-    //     let signer = tx.signer();
-    //     let slot = slots::mapping_slot(signer, tip_fee_manager::slots::USER_TOKENS);
-    //     println!("Submitting tx from {signer} with fee manager token slot 0x{slot:x}");
-    //
-    //     let res = node
-    //         .pool
-    //         .add_consensus_transaction(tx, TransactionOrigin::Local)
-    //         .await
-    //         .unwrap();
-    //     assert!(matches!(res.state, AddedTransactionState::Pending));
-    //     let pooled_tx = node.pool.get_transactions_by_sender(signer);
-    //     assert_eq!(pooled_tx.len(), 1);
-    //
-    //     let best = node.pool.best_transactions().next().unwrap();
-    //     assert_eq!(res.hash, *best.hash());
-    //
-    //
-    //
-    // TODO: assert state changes
+    // TODO: send 0 transfer
+
+    // TODO: assert state changes, gas spent in fee token
+
     Ok(())
 }
