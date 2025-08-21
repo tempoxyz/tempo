2f9fd190675d95707c954f311c4c1030c5f36c74
0xKitsune <0xKitsune@protonmail.com>
2025-08-21 14:00:50 -0400

    wip: use dev mode

--- crates/node/tests/tip20_transfer.rs
+++ crates/node/tests/tip20_transfer.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use alloy::{
     primitives::Address,
@@ -13,7 +13,7 @@ use alloy_rpc_types_eth::{TransactionInput, TransactionRequest};
 use reth_chainspec::ChainSpec;
 use reth_ethereum::{provider::db, tasks::TaskManager};
 use reth_node_builder::{NodeBuilder, NodeConfig, NodeHandle};
-use reth_node_core::args::RpcServerArgs;
+use reth_node_core::args::{DevArgs, RpcServerArgs};
 use tempo_node::node::TempoNode;
 use tempo_precompiles::{
     TIP_FEE_MANAGER_ADDRESS,
@@ -34,6 +34,7 @@ async fn test_tip20_transfer() -> eyre::Result<()> {
     let node_config = NodeConfig::test()
         .with_chain(Arc::new(chain_spec))
         .with_unused_ports()
+        .dev()
         .with_rpc(RpcServerArgs::default().with_unused_ports().with_http());
 
     let NodeHandle {
@@ -61,8 +62,12 @@ async fn test_tip20_transfer() -> eyre::Result<()> {
     let user_fee_token = fee_manager.userTokens(caller).call().await?;
     dbg!(user_fee_token);
 
-    // TODO: send 0 transfer
+    // TODO: get balance of fee token before
 
+    let tx = TransactionRequest::default().from(caller).to(caller);
+    let tx_hash = fee_manager.provider().send_transaction(tx).await?;
+
+    dbg!(tx_hash);
     // TODO: assert state changes, gas spent in fee token
 
     Ok(())
