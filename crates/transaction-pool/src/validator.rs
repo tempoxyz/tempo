d85f860e468b870ba704bd7051187832cf3c05cc
0xKitsune <0xKitsune@protonmail.com>
2025-08-25 12:38:08 -0400

    wip: fee token balance

--- crates/transaction-pool/src/validator.rs
+++ crates/transaction-pool/src/validator.rs
@@ -1,3 +1,5 @@
+use alloy_consensus::Transaction;
+use alloy_primitives::{Address, U256};
 use reth_chainspec::{ChainSpecProvider, EthereumHardforks};
 use reth_primitives_traits::{Block, SealedBlock};
 use reth_storage_api::StateProviderFactory;
@@ -5,12 +7,35 @@ use reth_transaction_pool::{
     EthPoolTransaction, EthTransactionValidator, TransactionOrigin, TransactionValidationOutcome,
     TransactionValidator,
 };
+use tempo_precompiles::{
+    TIP_FEE_MANAGER_ADDRESS,
+    contracts::{tip_fee_manager::TipFeeManager, types::IFeeManager},
+};
 
 /// Validator for Tempo transactions.
 #[derive(Debug, Clone)]
 pub struct TempoTransactionValidator<Client, Tx> {
     /// The type that performs the actual validation.
-    _inner: EthTransactionValidator<Client, Tx>,
+    inner: EthTransactionValidator<Client, Tx>,
+}
+
+impl<Client, Tx> TempoTransactionValidator<Client, Tx>
+where
+    Client: StateProviderFactory,
+    Tx: EthPoolTransaction,
+{
+    pub fn new(inner: EthTransactionValidator<Client, Tx>) -> Self {
+        Self { inner }
+    }
+
+    // async fn ensure_sufficient_balance(
+    //     &self,
+    //     transaction: &Tx,
+    // ) -> TransactionValidationOutcome<> {
+    //     let state_provider = self.inner.client().latest().expect("TODO: handle error ");
+    //
+    //     todo!()
+    // }
 }
 
 impl<Client, Tx> TransactionValidator for TempoTransactionValidator<Client, Tx>
@@ -22,35 +47,33 @@ where
 
     async fn validate_transaction(
         &self,
-        _origin: TransactionOrigin,
-        _transaction: Self::Transaction,
+        origin: TransactionOrigin,
+        transaction: Self::Transaction,
     ) -> TransactionValidationOutcome<Self::Transaction> {
-        // self.inner.validate_one(origin, transaction)
-        todo!()
+        self.inner.validate_one(origin, transaction)
     }
 
     async fn validate_transactions(
         &self,
-        _transactions: Vec<(TransactionOrigin, Self::Transaction)>,
+        transactions: Vec<(TransactionOrigin, Self::Transaction)>,
     ) -> Vec<TransactionValidationOutcome<Self::Transaction>> {
-        // self.inner.validate_batch(transactions)
-        todo!()
+        self.inner.validate_transactions(transactions).await
     }
 
     async fn validate_transactions_with_origin(
         &self,
-        _origin: TransactionOrigin,
-        _transactions: impl IntoIterator<Item = Self::Transaction> + Send,
+        origin: TransactionOrigin,
+        transactions: impl IntoIterator<Item = Self::Transaction> + Send,
     ) -> Vec<TransactionValidationOutcome<Self::Transaction>> {
-        // self.inner.validate_batch_with_origin(origin, transactions)
-        todo!()
+        self.inner
+            .validate_transactions_with_origin(origin, transactions)
+            .await
     }
 
-    fn on_new_head_block<B>(&self, _new_tip_block: &SealedBlock<B>)
+    fn on_new_head_block<B>(&self, new_tip_block: &SealedBlock<B>)
     where
         B: Block,
     {
-        // self.inner.on_new_head_block(new_tip_block.header())
-        todo!()
+        self.inner.on_new_head_block(new_tip_block)
     }
 }
