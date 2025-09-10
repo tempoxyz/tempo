4d74bb8e8e06eabd035ba6cfa207d5d0cb24673d
0xKitsune <0xkitsune@protonmail.com>
2025-09-10 16:46:25 -0400

    wip: next evm env

--- crates/evm/src/lib.rs
+++ crates/evm/src/lib.rs
@@ -6,7 +6,7 @@
 pub mod evm;
 use std::{borrow::Cow, convert::Infallible, sync::Arc};
 
-use alloy_eips::eip1559::INITIAL_BASE_FEE;
+use alloy_eips::{eip1559::INITIAL_BASE_FEE, eip7840::BlobParams};
 use alloy_evm::{EthEvmFactory, EvmFactory, eth::EthBlockExecutorFactory};
 use alloy_primitives::{Address, Bytes, U256};
 pub use evm::TempoEvmFactory;
@@ -26,7 +26,9 @@ use reth_primitives_traits::{
     AlloyBlockHeader, Header, SealedBlock, SealedHeader, constants::MAX_TX_GAS_LIMIT_OSAKA,
 };
 
-use reth_evm_ethereum::{EthBlockAssembler, RethReceiptBuilder, revm_spec};
+use reth_evm_ethereum::{
+    EthBlockAssembler, RethReceiptBuilder, revm_spec, revm_spec_by_timestamp_and_block_number,
+};
 use tempo_chainspec::TempoChainSpec;
 
 /// Tempo-related EVM configuration.
@@ -149,75 +151,83 @@ impl ConfigureEvm for TempoEvmConfig {
         parent: &Header,
         attributes: &NextBlockEnvAttributes,
     ) -> Result<EvmEnv, Self::Error> {
-        // // ensure we're not missing any timestamp based hardforks
-        // let chain_spec = self.executor_factory.chain_spec();
-        // let blob_params = chain_spec.blob_params_at_timestamp(attributes.timestamp);
-        // let spec_id = revm_spec_by_timestamp_and_block_number(
-        //     chain_spec,
-        //     attributes.timestamp,
-        //     parent.number() + 1,
-        // );
-        //
-        // // configure evm env based on parent block
-        // let mut cfg = CfgEnv::new()
-        //     .with_chain_id(chain_spec.chain().id())
-        //     .with_spec(spec_id);
-        //
-        // if let Some(blob_params) = &blob_params {
-        //     cfg.set_max_blobs_per_tx(blob_params.max_blobs_per_tx);
-        // }
-        //
-        // // if the parent block did not have excess blob gas (i.e. it was pre-cancun), but it is
-        // // cancun now, we need to set the excess blob gas to the default value(0)
-        // let blob_excess_gas_and_price = parent
-        //     .maybe_next_block_excess_blob_gas(blob_params)
-        //     .or_else(|| (spec_id == SpecId::CANCUN).then_some(0))
-        //     .map(|excess_blob_gas| {
-        //         let blob_gasprice = blob_params
-        //             .unwrap_or_else(|| alloy_eips::eip7840::BlobParams::cancun())
-        //             .calc_blob_fee(excess_blob_gas);
-        //         BlobExcessGasAndPrice {
-        //             excess_blob_gas,
-        //             blob_gasprice,
-        //         }
-        //     });
-        //
-        // let mut basefee = chain_spec.next_block_base_fee(parent, attributes.timestamp);
-        //
-        // let mut gas_limit = attributes.gas_limit;
-        //
-        // // If we are on the London fork boundary, we need to multiply the parent's gas limit by the
-        // // elasticity multiplier to get the new gas limit.
-        // if chain_spec
-        //     .fork(EthereumHardfork::London)
-        //     .transitions_at_block(parent.number + 1)
-        // {
-        //     let elasticity_multiplier = chain_spec
-        //         .base_fee_params_at_timestamp(attributes.timestamp)
-        //         .elasticity_multiplier;
-        //
-        //     // multiply the gas limit by the elasticity multiplier
-        //     gas_limit *= elasticity_multiplier as u64;
-        //
-        //     // set the base fee to the initial base fee from the EIP-1559 spec
-        //     basefee = Some(INITIAL_BASE_FEE)
-        // }
-        //
-        // let block_env = BlockEnv {
-        //     number: U256::from(parent.number + 1),
-        //     beneficiary: attributes.suggested_fee_recipient,
-        //     timestamp: U256::from(attributes.timestamp),
-        //     difficulty: U256::ZERO,
-        //     prevrandao: Some(attributes.prev_randao),
-        //     gas_limit,
-        //     // calculate basefee based on parent block's gas usage
-        //     basefee: basefee.unwrap_or_default(),
-        //     // calculate excess gas based on parent block's blob gas usage
-        //     blob_excess_gas_and_price,
-        // };
-        //
-        // Ok((cfg, block_env).into())
-        todo!()
+        // ensure we're not missing any timestamp based hardforks
+        let chain_spec = self.chain_spec();
+        let blob_params = chain_spec.blob_params_at_timestamp(attributes.timestamp);
+        let spec_id = revm_spec_by_timestamp_and_block_number(
+            chain_spec,
+            attributes.timestamp,
+            parent.number() + 1,
+        );
+
+        // configure evm env based on parent block
+        let mut cfg = CfgEnv::new()
+            .with_chain_id(self.chain_spec().chain().id())
+            .with_spec(spec_id);
+
+        if let Some(blob_params) = &blob_params {
+            cfg.set_max_blobs_per_tx(blob_params.max_blobs_per_tx);
+        }
+
+        if self
+            .chain_spec()
+            .is_osaka_active_at_timestamp(attributes.timestamp)
+        {
+            cfg.tx_gas_limit_cap = Some(MAX_TX_GAS_LIMIT_OSAKA);
+        }
+
+        // if the parent block did not have excess blob gas (i.e. it was pre-cancun), but it is
+        // cancun now, we need to set the excess blob gas to the default value(0)
+        let blob_excess_gas_and_price = parent
+            .maybe_next_block_excess_blob_gas(blob_params)
+            .or_else(|| (spec_id == SpecId::CANCUN).then_some(0))
+            .map(|excess_blob_gas| {
+                let blob_gasprice = blob_params
+                    .unwrap_or_else(BlobParams::cancun)
+                    .calc_blob_fee(excess_blob_gas);
+                BlobExcessGasAndPrice {
+                    excess_blob_gas,
+                    blob_gasprice,
+                }
+            });
+
+        let mut basefee = chain_spec.next_block_base_fee(parent, attributes.timestamp);
+
+        let mut gas_limit = attributes.gas_limit;
+
+        // If we are on the London fork boundary, we need to multiply the parent's gas limit by the
+        // elasticity multiplier to get the new gas limit.
+        if self
+            .chain_spec()
+            .fork(EthereumHardfork::London)
+            .transitions_at_block(parent.number + 1)
+        {
+            let elasticity_multiplier = self
+                .chain_spec()
+                .base_fee_params_at_timestamp(attributes.timestamp)
+                .elasticity_multiplier;
+
+            // multiply the gas limit by the elasticity multiplier
+            gas_limit *= elasticity_multiplier as u64;
+
+            // set the base fee to the initial base fee from the EIP-1559 spec
+            basefee = Some(INITIAL_BASE_FEE)
+        }
+
+        let block_env = BlockEnv {
+            number: U256::from(parent.number + 1),
+            beneficiary: attributes.suggested_fee_recipient,
+            timestamp: U256::from(attributes.timestamp),
+            difficulty: U256::ZERO,
+            prevrandao: Some(attributes.prev_randao),
+            gas_limit,
+            // calculate basefee based on parent block's gas usage
+            basefee: basefee.unwrap_or_default(),
+            // calculate excess gas based on parent block's blob gas usage
+            blob_excess_gas_and_price,
+        };
+
+        Ok((cfg, block_env).into())
     }
 
     fn context_for_block<'a>(&self, block: &'a SealedBlock<Block>) -> EthBlockExecutionCtx<'a> {
