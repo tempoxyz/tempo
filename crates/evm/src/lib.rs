2132ea06c9a528224ef9446a707f5b7953d450ff
0xKitsune <0xkitsune@protonmail.com>
2025-09-10 15:46:37 -0400

    wip: tempo evm config

--- crates/evm/src/lib.rs
+++ crates/evm/src/lib.rs
@@ -4,8 +4,22 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 pub mod evm;
+use std::sync::Arc;
+
+use alloy_evm::{EthEvmFactory, EvmFactory, eth::EthBlockExecutorFactory};
 pub use evm::TempoEvmFactory;
 
+use reth_evm_ethereum::{EthBlockAssembler, RethReceiptBuilder};
 use tempo_chainspec::TempoChainSpec;
 
-pub type TempoEvmConfig = reth_evm_ethereum::EthEvmConfig<TempoChainSpec, TempoEvmFactory>;
+// pub type TempoEvmConfig = reth_evm_ethereum::EthEvmConfig<TempoChainSpec, TempoEvmFactory>;
+
+/// Tempo-related EVM configuration.
+#[derive(Debug, Clone)]
+pub struct TempoEvmConfig {
+    /// Inner [`EthBlockExecutorFactory`].
+    pub executor_factory:
+        EthBlockExecutorFactory<RethReceiptBuilder, Arc<TempoChainSpec>, EthEvmFactory>,
+    /// Ethereum block assembler.
+    pub block_assembler: EthBlockAssembler<TempoChainSpec>,
+}
