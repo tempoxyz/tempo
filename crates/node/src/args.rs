8e4e08afe0fb3264978c7983a9e0f62d4637fbb9
Arsenii Kulikov <klkvrr@gmail.com>
2025-09-10 19:30:46 +0300

    refactor: move commonware setup into main tempo binary

--- crates/node/src/args.rs
+++ crates/node/src/args.rs
@@ -1,4 +1,3 @@
-use reth_malachite::cli::MalachiteArgs;
 use tempo_faucet::args::FaucetArgs;
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, clap::Args)]
@@ -7,8 +6,8 @@ pub struct TempoArgs {
     #[arg(long)]
     pub no_consensus: bool,
 
-    #[command(flatten)]
-    pub malachite_args: MalachiteArgs,
+    #[clap(long, value_name = "FILE")]
+    pub consensus_config: camino::Utf8PathBuf,
 
     #[command(flatten)]
     pub faucet_args: FaucetArgs,
