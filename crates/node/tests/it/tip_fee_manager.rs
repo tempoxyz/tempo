1733773bd30531bf6ad86bc03b9e05aa0eb5c435
Arsenii Kulikov <klkvrr@gmail.com>
2025-09-23 23:37:16 +0400

    wip

--- crates/node/tests/it/tip_fee_manager.rs
+++ crates/node/tests/it/tip_fee_manager.rs
@@ -1,10 +1,17 @@
 use crate::utils::{setup_test_node, setup_test_token};
-use alloy::{providers::ProviderBuilder, signers::local::MnemonicBuilder};
+use alloy::{
+    providers::{Provider, ProviderBuilder},
+    signers::local::MnemonicBuilder,
+};
+use alloy_eips::Encodable2718;
+use alloy_network::TxSignerSync;
+use alloy_primitives::Address;
 use std::env;
 use tempo_precompiles::{
     TIP_FEE_MANAGER_ADDRESS,
     contracts::{token_id_to_address, types::IFeeManager},
 };
+use tempo_primitives::TxFeeToken;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_set_user_token() -> eyre::Result<()> {
@@ -80,6 +87,50 @@ async fn test_set_validator_token() -> eyre::Result<()> {
         .call()
         .await?;
     assert_eq!(current_token, *validator_token.address());
+
+    Ok(())
+}
+
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_fee_token_tx() -> eyre::Result<()> {
+    reth_tracing::init_test_tracing();
+
+    let source = if let Ok(rpc_url) = env::var("RPC_URL") {
+        crate::utils::NodeSource::ExternalRpc(rpc_url.parse()?)
+    } else {
+        crate::utils::NodeSource::LocalNode(include_str!("../assets/test-genesis.json").to_string())
+    };
+    let (http_url, _local_node) = setup_test_node(source).await?;
+
+    let wallet = MnemonicBuilder::from_phrase(crate::utils::TEST_MNEMONIC).build()?;
+    let user_address = wallet.address();
+    let provider = ProviderBuilder::new().wallet(wallet.clone()).connect_http(http_url);
+
+    let user_token = setup_test_token(provider.clone(), user_address).await?;
+
+    let fees = provider.estimate_eip1559_fees().await?;
+
+    let mut tx = TxFeeToken {
+        chain_id: provider.get_chain_id().await?,
+        nonce: provider.get_transaction_count(user_address).await?,
+        fee_token: Some(*user_token.address()),
+        max_priority_fee_per_gas: fees.max_priority_fee_per_gas,
+        max_fee_per_gas: fees.max_fee_per_gas,
+        gas_limit: 21000,
+        to: Address::ZERO.into(),
+        ..Default::default()
+    };
+
+    let signature = wallet.sign_transaction_sync(&mut tx)?;
+
+    let tx = tx.into_signed(signature);
+
+    provider
+        .send_raw_transaction(&tx.encoded_2718())
+        .await?
+        .get_receipt()
+        .await?;
 
     Ok(())
 }
