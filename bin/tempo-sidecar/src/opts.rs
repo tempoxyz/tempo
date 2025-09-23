09940bb49873c08d7c810e0332c107c32eb9fe3e
Zygimantas <zygis@skip.build>
2025-09-23 14:31:56 +0200

    feat(sidecar): move monitor as a sidecar subcommand

---
+++ bin/tempo-sidecar/src/opts.rs
@@ -0,0 +1,15 @@
+use clap::{Parser, Subcommand};
+use crate::cmd::monitor::MonitorArgs;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct TempoSidecar {
+    // TODO: add node args
+    #[command(subcommand)]
+    pub cmd: TempoSidecarSubcommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum TempoSidecarSubcommand {
+    FeeAMMMonitor(MonitorArgs),
+}
