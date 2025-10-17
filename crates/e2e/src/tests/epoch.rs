6c66ef0876dadd115512ce696b727d56b33d3f11
Richard Janis Goldschmidt <github@aberrat.io>
2025-10-17 13:46:53 +0200

    add epoch transition test with fallible links

--- crates/e2e/src/tests/epoch.rs
+++ crates/e2e/src/tests/epoch.rs
@@ -6,12 +6,43 @@ use commonware_p2p::simulated::Link;
 use crate::{Setup, run};
 
 #[test_traced]
-fn transitions_three_epochs() {
+fn transitions_with_perfect_links() {
     let _ = tempo_eyre::install();
     let linkage = Link {
         latency: Duration::from_millis(10),
         jitter: Duration::from_millis(1),
         success_rate: 1.0,
+    };
+
+    let setup = Setup {
+        how_many: 5,
+        seed: 0,
+        linkage,
+        heights_per_epoch: 2,
+    };
+
+    let mut epoch_reached = false;
+    let mut height_reached = false;
+    let _first = run(setup, move |metric, value| {
+        if metric.ends_with("_orchestrator_latest_epoch") {
+            let value = value.parse::<u64>().unwrap();
+            epoch_reached |= value >= 3;
+        }
+        if metric.ends_with("_sync_processed_height") {
+            let value = value.parse::<u64>().unwrap();
+            height_reached |= value >= 6;
+        }
+        epoch_reached && height_reached
+    });
+}
+
+#[test_traced]
+fn transitions_with_fallible_links() {
+    let _ = tempo_eyre::install();
+    let linkage = Link {
+        latency: Duration::from_millis(10),
+        jitter: Duration::from_millis(1),
+        success_rate: 0.9,
     };
 
     let setup = Setup {
