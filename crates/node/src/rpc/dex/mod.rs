7af0a5cba1602b567c47982ea3eddd3e26df660b
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-10-24 13:11:51 +0200

    feat(node): Launch node with `TempoDex` RPC module

--- crates/node/src/rpc/dex/mod.rs
+++ crates/node/src/rpc/dex/mod.rs
@@ -3,6 +3,8 @@ pub use types::{
 };
 
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use reth_node_core::rpc::result::internal_rpc_err;
+use reth_rpc_eth_api::RpcNodeCore;
 
 pub mod types;
 
@@ -10,4 +12,30 @@ pub mod types;
 pub trait TempoDexApi {
     #[method(name = "getOrders")]
     async fn orders(&self, params: Vec<OrdersParams>) -> RpcResult<OrdersResponse>;
+}
+
+/// The JSON-RPC handlers for the `dex_` namespace.
+#[derive(Debug, Clone, Default)]
+pub struct TempoDex<EthApi> {
+    eth_api: EthApi,
+}
+
+impl<EthApi> TempoDex<EthApi> {
+    pub fn new(eth_api: EthApi) -> Self {
+        Self { eth_api }
+    }
+}
+
+#[async_trait::async_trait]
+impl<EthApi: RpcNodeCore> TempoDexApiServer for TempoDex<EthApi> {
+    async fn orders(&self, _params: Vec<OrdersParams>) -> RpcResult<OrdersResponse> {
+        Err(internal_rpc_err("unimplemented"))
+    }
+}
+
+impl<EthApi: RpcNodeCore> TempoDex<EthApi> {
+    /// Access the underlying provider.
+    pub fn provider(&self) -> &EthApi::Provider {
+        self.eth_api.provider()
+    }
 }
