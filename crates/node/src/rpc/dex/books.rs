ac30070ad43f58aef2a1bd2b7edbac35a6cc9bec
Roman Hodulák <hodulakr@gmail.com>
2025-10-24 16:13:59 +0200

    feat(node): Make all fields public in the `dex_getOrderbooks` schema

--- crates/node/src/rpc/dex/books.rs
+++ crates/node/src/rpc/dex/books.rs
@@ -11,39 +11,39 @@ pub struct OrderbooksParam {}
 #[serde(rename_all = "camelCase")]
 pub struct OrderbooksResponse {
     /// Cursor for next page, null if no more results
-    next_cursor: Option<String>,
+    pub next_cursor: Option<String>,
     /// Orderbooks that match the query
-    orderbooks: Vec<Orderbook>,
+    pub orderbooks: Vec<Orderbook>,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Orderbook {
     /// Base token address
-    base_token: Address,
+    pub base_token: Address,
     /// Orderbook key (keccak256 of base and quote tokens)
-    book_key: B256,
+    pub book_key: B256,
     /// Best ask tick (lowest ask price)
-    best_ask_tick: Tick,
+    pub best_ask_tick: Tick,
     /// Best bid tick (highest bid price)
-    best_bid_tick: Tick,
+    pub best_bid_tick: Tick,
     /// Quote token address
-    quote_token: Address,
+    pub quote_token: Address,
     /// Spread in ticks (best_ask_tick - best_bid_tick)
-    spread: Tick,
+    pub spread: Tick,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct OrderbooksFilter {
     /// Filter by base token address
-    base_token: Option<Address>,
+    pub base_token: Option<Address>,
     /// Best ask tick in range
-    best_ask_tick: Option<FilterRange<Tick>>,
+    pub best_ask_tick: Option<FilterRange<Tick>>,
     /// Best bid tick in range
-    best_bid_tick: Option<FilterRange<Tick>>,
+    pub best_bid_tick: Option<FilterRange<Tick>>,
     /// Filter by quote token address
-    quote_token: Option<Address>,
+    pub quote_token: Option<Address>,
     /// Spread in range (in ticks)
-    spread: Option<FilterRange<Tick>>,
+    pub spread: Option<FilterRange<Tick>>,
 }
