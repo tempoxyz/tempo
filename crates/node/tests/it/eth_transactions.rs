686fe2d6ece451f83437a1dba2235288d62207b9
Matthias Seitz <matthias.seitz@outlook.de>
2025-11-05 14:10:07 +0100

    bump gas

--- crates/node/tests/it/eth_transactions.rs
+++ crates/node/tests/it/eth_transactions.rs
@@ -32,7 +32,7 @@ async fn test_get_transaction_by_sender_and_nonce() -> eyre::Result<()> {
     let pending_tx = token
         .mint(caller, mint_amount)
         .gas_price(TEMPO_BASE_FEE as u128)
-        .gas(30000)
+        .gas(300_000)
         .send()
         .await?;
 
