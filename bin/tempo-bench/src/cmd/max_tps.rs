20443cc944e6911bd43b2b994a2c3a9d067a71a5
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-11-25 12:44:17 +0000

    Revert "revertme: print account nonce"
    This reverts commit 24098da291de05759a228955d1fe773d87cb638f.

--- bin/tempo-bench/src/cmd/max_tps.rs
+++ bin/tempo-bench/src/cmd/max_tps.rs
@@ -520,11 +520,6 @@ async fn fund_accounts(
         assert_receipts(tx_hashes, max_concurrent_requests).await?
     }
 
-    println!(
-        "nonce: {:?}",
-        provider.get_account(addresses[0]).await?.nonce
-    );
-
     progress.finish();
     println!("Finished funding accounts");
     Ok(())
