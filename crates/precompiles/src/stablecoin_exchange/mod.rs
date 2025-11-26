ebc05e5a4ad7cf301a90156c5dbb658f160ea582
0xKitsune <0xkitsune@protonmail.com>
2025-11-26 12:42:19 -0500

    fix: enforce tick spacing

--- crates/precompiles/src/stablecoin_exchange/mod.rs
+++ crates/precompiles/src/stablecoin_exchange/mod.rs
@@ -458,7 +458,7 @@ impl<'a, S: PrecompileStorageProvider> StablecoinExchange<'a, S> {
         }
 
         // Post allegretto, enforce that the tick adheres to tick spacing
-        if self.storage.spec().is_allegretto() && (tick as u64).is_multiple_of(TICK_SPACING) {
+        if self.storage.spec().is_allegretto() && !(tick as u64).is_multiple_of(TICK_SPACING) {
             return Err(StablecoinExchangeError::invalid_tick().into());
         }
 
@@ -548,7 +548,7 @@ impl<'a, S: PrecompileStorageProvider> StablecoinExchange<'a, S> {
         }
 
         // Post allegretto, enforce that the tick adheres to tick spacing
-        if self.storage.spec().is_allegretto() && (tick as u64).is_multiple_of(TICK_SPACING) {
+        if self.storage.spec().is_allegretto() && !(tick as u64).is_multiple_of(TICK_SPACING) {
             return Err(StablecoinExchangeError::invalid_tick().into());
         }
 
@@ -557,7 +557,7 @@ impl<'a, S: PrecompileStorageProvider> StablecoinExchange<'a, S> {
         }
 
         // Post allegretto, enforce that the tick adheres to tick spacing
-        if self.storage.spec().is_allegretto() && (tick as u64).is_multiple_of(TICK_SPACING) {
+        if self.storage.spec().is_allegretto() && !(tick as u64).is_multiple_of(TICK_SPACING) {
             return Err(StablecoinExchangeError::invalid_flip_tick().into());
         }
 
