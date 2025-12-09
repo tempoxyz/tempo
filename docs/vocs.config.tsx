a96e3fadb53cf39c6fe4711bf6a7a374988179ee
Dan Robinson <danrobinson010@gmail.com>
2025-12-09 19:33:40 +0400

    remove card, put in sidebar

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -317,6 +317,10 @@ export default defineConfig({
                 link: '/protocol/transactions/spec-tempo-transaction',
               },
               {
+                text: 'Account Keychain Precompile Specification',
+                link: '/protocol/transactions/AccountKeychain',
+              },
+              {
                 text: 'Default Account Abstraction Specification',
                 link: '/protocol/transactions/spec-default-aa',
               },
@@ -1674,13 +1678,13 @@ export default defineConfig({
     process.env['DISABLE_TWOSLASH']! === 'true'
       ? false
       : {
-          compilerOptions: {
-            moduleResolution: ModuleResolutionKind.Bundler,
-          },
-          extraFiles: {
-            'twoslash-env.d.ts': twoslashSupportFile,
-          },
+        compilerOptions: {
+          moduleResolution: ModuleResolutionKind.Bundler,
         },
+        extraFiles: {
+          'twoslash-env.d.ts': twoslashSupportFile,
+        },
+      },
   vite: {
     plugins: [
       {
