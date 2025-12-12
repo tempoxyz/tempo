f739cfc7fdfa0f43017b9124a96267ce5fa7b9d6
howy <132113803+howydev@users.noreply.github.com>
2025-12-12 21:09:52 +0800

    fix: lint

--- xtask/src/genesis_args.rs
+++ xtask/src/genesis_args.rs
@@ -26,8 +26,7 @@ use tempo_commonware_node_config::{Peers, PublicPolynomial, SigningKey, SigningS
 use tempo_contracts::{
     ARACHNID_CREATE2_FACTORY_ADDRESS, CREATEX_ADDRESS, DEFAULT_7702_DELEGATE_ADDRESS,
     MULTICALL_ADDRESS, PERMIT2_ADDRESS, SAFE_DEPLOYER_ADDRESS,
-    contracts::ARACHNID_CREATE2_FACTORY_BYTECODE,
-    contracts::CREATEX_POST_ALLEGRO_MODERATO_BYTECODE,
+    contracts::{ARACHNID_CREATE2_FACTORY_BYTECODE, CREATEX_POST_ALLEGRO_MODERATO_BYTECODE},
     precompiles::{ITIP20Factory, IValidatorConfig},
 };
 use tempo_dkg_onchain_artifacts::PublicOutcome;
