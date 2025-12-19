7499e4a9c51e3c1fa0c1148e5c2957be72d98a1c
o-az <omaraziz.dev@proton.me>
2025-12-19 10:56:04 -0800

    feat: tokenlist page

--- docs/vocs.config.tsx
+++ docs/vocs.config.tsx
@@ -133,6 +133,10 @@ export default defineConfig({
             link: '/quickstart/predeployed-contracts',
           },
           {
+            text: 'Token List Registry',
+            link: '/quickstart/tokenlist',
+          },
+          {
             text: 'Wallet Developers',
             link: '/quickstart/wallet-developers',
           },
