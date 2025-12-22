f21b8f9c6dfcb3385644804f797852534827dc91
Arsenii Kulikov <klkvrr@gmail.com>
2025-12-22 16:38:47 +0400

    add doc

--- crates/revm/src/handler.rs
+++ crates/revm/src/handler.rs
@@ -946,6 +946,8 @@ where
             })?;
         }
 
+        // Short-circuit if there is no spending for this transaction and `collectFeePreTx`
+        // call will not collect any fees.
         if gas_balance_spending.is_zero() {
             evm.nonce_2d_gas = nonce_2d_gas;
             return Ok(());
