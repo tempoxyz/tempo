6a2ba3f2147b208aaac5e73846081be3ee830a4c
Hamdi Allam <hamdi.allam97@gmail.com>
2026-01-28 14:34:45 -0500

    unneeded return

--- bin/tempo/src/defaults.rs
+++ bin/tempo/src/defaults.rs
@@ -82,7 +82,7 @@ fn extract_telemetry_url(args: &[String]) -> Option<String> {
         }
     }
 
-    return None;
+    None
 }
 
 /// Expands `--telemetry-url` (or `TELEMETRY_URL` env var) into the equivalent telemetry arguments.
