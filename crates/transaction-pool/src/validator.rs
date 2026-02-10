b4e2ffddd98fb21b7782859114f85462f98ba72c
0xKitsune <0xKitsune@protonmail.com>
2026-02-10 08:51:18 -0500

    fix: update to use spec.gas_existing_nonce_key()

--- crates/transaction-pool/src/validator.rs
+++ crates/transaction-pool/src/validator.rs
@@ -410,7 +410,7 @@ where
             } else if !tx.nonce_key.is_zero() {
                 // Existing 2D nonce key (nonce > 0): cold SLOAD + warm SSTORE reset
                 // TIP-1000 Invariant 3: existing state updates charge 5,000 gas
-                init_and_floor_gas.initial_gas += EXISTING_NONCE_KEY_GAS;
+                init_and_floor_gas.initial_gas += spec.gas_existing_nonce_key();
             }
             // In CREATE tx with 2d nonce, check if account.nonce is 0, if so, add 250,000 gas.
             // This covers caller creation of account.
