906ae90338e9f384c95bced1244f2f4638aba163
howydev <132113803+howydev@users.noreply.github.com>
2026-03-07 21:10:58 -0500

    .

--- tips/ref-impls/test/ValidatorConfigV2.t.sol
+++ tips/ref-impls/test/ValidatorConfigV2.t.sol
@@ -469,7 +469,8 @@ contract ValidatorConfigV2Test is BaseTest {
                 validator3,
                 _signAdd(PRIV_KEY_2, validator3, ingress3, egress3, validator3)
             );
-            try validatorConfigV2.rotateValidator(4, PUB_KEY_3, ingress3, egress3, hex"0000") {
+            string memory uniqueIngress = "10.0.0.3:8000";
+            try validatorConfigV2.rotateValidator(4, PUB_KEY_3, uniqueIngress, egress3, hex"0000") {
                 revert CallShouldHaveReverted();
             } catch (bytes memory err) {
                 assertEq(err, abi.encodeWithSelector(InvalidSignatureFormat.selector));
