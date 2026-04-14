2e4574ee89ab2356cc3a96bb306c1b4ef0b3741e
Richard Janis Goldschmidt <github@aberrat.io>
2026-04-14 16:39:27 +0200

    provide a refinement ed25519 public key for tempo context header

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -6,6 +6,8 @@
 
 pub use alloy_consensus::Header;
 
+pub mod ed25519;
+
 pub mod transaction;
 pub use transaction::{
     AASigned, MAX_WEBAUTHN_SIGNATURE_LENGTH, P256_SIGNATURE_LENGTH, SECP256K1_SIGNATURE_LENGTH,
