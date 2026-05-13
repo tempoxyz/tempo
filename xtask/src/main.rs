31e578ef6ee314778ce73465173c3f1b3adc7386
Alexey Shekhirin <github@shekhirin.com>
2026-05-13 08:54:27 +0000

    chore(cli): remove legacy state bloat commands

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,8 +3,7 @@ use std::net::SocketAddr;
 
 use crate::{
     check_abi::CheckAbi, generate_devnet::GenerateDevnet, generate_genesis::GenerateGenesis,
-    generate_localnet::GenerateLocalnet, generate_state_bloat::GenerateStateBloat,
-    get_dkg_outcome::GetDkgOutcome,
+    generate_localnet::GenerateLocalnet, get_dkg_outcome::GetDkgOutcome,
 };
 
 use alloy::signers::{local::MnemonicBuilder, utils::secret_key_to_address};
@@ -16,7 +15,6 @@ mod check_abi;
 mod generate_devnet;
 mod generate_genesis;
 mod generate_localnet;
-mod generate_state_bloat;
 mod genesis_args;
 mod get_dkg_outcome;
 
@@ -36,10 +34,6 @@ async fn main() -> eyre::Result<()> {
             .await
             .wrap_err("failed to generate localnet configs"),
         Action::GenerateAddPeer(cfg) => generate_config_to_add_peer(cfg),
-        Action::GenerateStateBloat(args) => args
-            .run()
-            .await
-            .wrap_err("failed to generate state bloat file"),
     }
 }
 
@@ -61,7 +55,6 @@ enum Action {
     GenerateDevnet(GenerateDevnet),
     GenerateLocalnet(GenerateLocalnet),
     GenerateAddPeer(GenerateAddPeer),
-    GenerateStateBloat(GenerateStateBloat),
 }
 
 #[derive(Debug, clap::Args)]
