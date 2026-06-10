7bbe7abcc44c5f92308f1e140a6fc192a3f1912a
0xrusowsky <0xrusowsky@proton.me>
2026-06-10 10:32:54 +0200

    style: clippy

--- crates/revm/src/handler.rs
+++ crates/revm/src/handler.rs
@@ -1536,7 +1536,6 @@ where
         evm: &mut Self::Evm,
         exec_result: &mut FrameResult,
     ) -> Result<(), Self::Error> {
-        let gas = exec_result.gas_mut();
         // Call collectFeePostTx on TipFeeManager precompile
         let context = &mut evm.inner.ctx;
         let tx = context.tx();
