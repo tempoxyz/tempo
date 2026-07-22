c7d3ec73be84da9a421743a30f6d86113d7158d3
Derek Cofausper <256792747+decofe@users.noreply.github.com>
2026-07-22 21:08:27 +0000

    test(precompiles): drop zone factory export test

--- crates/precompiles/tests/zone_factory.rs
+++
@@ -1,5 +0,0 @@
-#[test]
-fn exports_zone_portal_storage_slots() {
-    let _ = tempo_precompiles::zone_factory::zone_portal_slots::ADMIN;
-    let _ = tempo_precompiles::zone_factory::zone_portal_slots::TOKEN_CONFIGS;
-}
