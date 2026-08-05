8127010a10516a563792afecb146bee90f2dc56e
Derek Cofausper <256792747+decofe@users.noreply.github.com>
2026-08-05 04:07:16 +0000

    fix: resolve breaking changes from reth update (2026-08-05)

--- crates/node/src/rpc/mod.rs
+++ crates/node/src/rpc/mod.rs
@@ -16,7 +16,9 @@ pub use fork_schedule::{TempoForkScheduleApiServer, TempoForkScheduleRpc};
 use futures::{TryFutureExt, future::Either};
 pub use operator::{TempoOperatorApiServer, TempoOperatorRpc};
 use reth_errors::RethError;
-use reth_primitives_traits::{HeaderTy, Recovered, TransactionMeta, WithEncoded};
+use reth_primitives_traits::{
+    BlockTy, HeaderTy, Recovered, SealedBlock, SealedHeaderFor, TransactionMeta, WithEncoded,
+};
 use reth_rpc_eth_api::{FromEthApiError, IntoEthApiError, RpcTxReq};
 use reth_transaction_pool::{PoolTransaction, PoolTx, TransactionOrigin, TransactionPool};
 pub use simulate::{TempoSimulate, TempoSimulateApiServer, TempoSimulateV1Response};
@@ -568,15 +570,26 @@ where
     ChainSpec: EthChainSpec + 'static,
 {
     type RpcReceipt = TempoTransactionReceipt;
+    type RpcLog = Log;
     type Error = EthApiError;
+
+    fn convert_log(
+        &self,
+        log: Log,
+        _receipt: &TempoReceipt,
+        _header: &SealedHeaderFor<TempoPrimitives>,
+    ) -> Result<Self::RpcLog, Self::Error> {
+        Ok(log)
+    }
 
     fn convert_receipts(
         &self,
         receipts: Vec<ConvertReceiptInput<'_, TempoPrimitives>>,
+        block: &SealedBlock<BlockTy<TempoPrimitives>>,
     ) -> Result<Vec<Self::RpcReceipt>, Self::Error> {
         let receipt_context = receipts.iter().map(|r| r.tx).collect::<Vec<_>>();
         self.inner
-            .convert_receipts(receipts)?
+            .convert_receipts(receipts, block)?
             .into_iter()
             .zip(receipt_context)
             .map(|(inner, tx)| {
