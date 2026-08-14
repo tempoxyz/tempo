397863e47860f72115850f1fa604ed540c3ad924
Arsenii Kulikov <klkvrr@gmail.com>
2026-08-14 20:28:33 +0800

    fix size

--- crates/primitives/src/transaction/tt_authorization.rs
+++ crates/primitives/src/transaction/tt_authorization.rs
@@ -117,7 +117,7 @@ impl TempoSignedAuthorization {
 
     /// Calculates a heuristic for the in-memory size of this authorization
     pub fn size(&self) -> usize {
-        size_of::<Self>() + self.signature.size()
+        size_of::<Authorization>() + self.signature.size()
     }
 }
 
