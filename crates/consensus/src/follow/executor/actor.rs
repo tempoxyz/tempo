8c46984c9fe31db402becb9bfb84f836e50faee7
Hamdi Allam <hamdi.allam97@gmail.com>
2026-08-28 14:59:35 -0400

    comment on last/pending fcu

--- crates/consensus/src/follow/executor/actor.rs
+++ crates/consensus/src/follow/executor/actor.rs
@@ -39,7 +39,9 @@ pub(crate) struct Actor<TContext, P, E, M = crate::alias::marshal::Mailbox> {
     epoch_strategy: FixedEpocher,
     floor: Height,
 
+    // The last completed FCU
     last_fcu: Forkchoice,
+    // Next FCU to be delivered when it supersed the last
     pending_fcu: Forkchoice,
 
     block_queue: VecDeque<(Block, Exact)>,
