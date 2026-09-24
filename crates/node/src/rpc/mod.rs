b813bcf4b8f1bc0a5e0511c2a1f4408812b92822
Arsenii Kulikov <klkvrr@gmail.com>
2026-09-24 21:20:30 +0300

    refactor(rpc): reuse default transaction environment

--- crates/node/src/rpc/mod.rs
+++ crates/node/src/rpc/mod.rs
@@ -16,7 +16,7 @@ pub use fork_schedule::{TempoForkScheduleApiServer, TempoForkScheduleRpc};
 use futures::TryFutureExt;
 pub use operator::{TempoOperatorApiServer, TempoOperatorRpc};
 use reth_primitives_traits::{HeaderTy, SealedHeaderFor, TransactionMeta, WithEncoded};
-use reth_rpc_eth_api::{FromEthApiError, IntoEthApiError, RpcConvert, RpcTxReq};
+use reth_rpc_eth_api::{FromEthApiError, IntoEthApiError, RpcTxReq};
 use reth_transaction_pool::{PoolTransaction, PoolTx, TransactionOrigin};
 pub use simulate::{TempoSimulate, TempoSimulateApiServer, TempoSimulateV1Response};
 use std::{marker::PhantomData, sync::Arc};
@@ -427,17 +427,7 @@ where
             request.nonce = Some(nonce);
         }
 
-        if request.nonce.is_none() {
-            request.nonce = Some(
-                db.get_account(&request.from.unwrap_or_default())
-                    .map_err(Into::into)
-                    .map_err(Self::Error::from_eth_err)?
-                    .map(|account| account.nonce)
-                    .unwrap_or_default(),
-            );
-        }
-
-        Ok(self.converter().tx_env(request, evm_env)?)
+        Ok(self.inner.create_txn_env(evm_env, request, db)?)
     }
 }
 
